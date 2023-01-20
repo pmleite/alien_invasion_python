5e131e712870ce15ce953aadaa0e123ff08cd6fb
pmleite <paulo.miguel.r.leite@gmail.com>
2023-01-20 12:44:06 -0100

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,67 +1,84 @@
 import sys
 import pygame
-from settings import Settings
 from ship import Ship
+from settings import Settings
+from bullet import Bullet
 
 class AlienInvasion:
 
     def __init__(self):
-        
         pygame.init()
-        self.settings = Settings()
-        self.bg_image = pygame.image.load(self.settings.bg_image)
-        self.screen   = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-        self.settings.screen_width  = self.screen.get_rect().width
-        self.settings.screen_heigth = self.screen.get_rect().height
+        self.settings      = Settings()
+        self.bg_image      = pygame.image.load(self.settings.bg_image)
+        self.screen        = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen_width  = self.screen.get_rect().width
+        self.screen_height = self.screen.get_rect().height
         pygame.display.set_caption(self.settings.caption)
-        self.ship     = Ship(self)
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
-    def run_game(self):
+    def run(self):
         while True:
             self._check_events()
-            self.ship.update(self)
+            self.ship.update()
+            self.bullets.update()
             self._update_screen()
-            
-
-    def _check_events(self):
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                
-                # Inicia Movimento (Muda as flags para true) 
-                elif event.type == pygame.KEYDOWN:
-                    self._check_keydown_events(event)
-
-                # Interrompe o movimento (Muda as flags para false)  
-                elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)
-
     
 
+    def _check_events(self):
+        for event in pygame.event.get():   
+               
+                if event.type == pygame.QUIT:
+                    sys.exit()
 
+                elif event.type == pygame.KEYDOWN:
+                    self.check_key_down_events(event)
+                elif event.type == pygame.KEYUP:
+                    self.check_key_up_events(event)
 
-    def _check_keydown_events(self, event):
+    def check_key_down_events(self, event):
         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = True
+            self.ship.moving_rigth = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
-    
-    def _check_keyup_events(self, event):
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def check_key_up_events(self, event):
         if event.key == pygame.K_RIGHT:
-            self.ship.moving_right = False  
+            self.ship.moving_rigth = False
         elif event.key == pygame.K_LEFT:
-            self.ship.moving_left = False  
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
 
     def _update_screen(self):
-            self.screen.blit(self.bg_image, (0,0))     
-            self.ship.blitme()   
-            pygame.display.flip()
+        """ Vai atualizar o ecrã """
+        self.screen.blit(self.bg_image, (0,0))
+        self.ship.blitme()
 
-if __name__ == "__main__":
-    ai = AlienInvasion()
-    ai.run_game()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        pygame.display.flip()
+    
+              
+            
+if __name__ == '__main__':
+    
+    jogo = AlienInvasion()
+    jogo.run()
+
+
+
+
+
+
+
 
 
 
--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
 class Settings:
-
+   
     def __init__(self):
-        self.screen_width  = 0
-        self.screen_heigth = 0
-        self.bg_image      = "images/background.jpeg"
-        self.ship_image    = "images/ship.png"
-        self.caption       = "Alien Invasion by Paulo Leite"
-        self.ship_speed    = 0.8
+
+        self.bg_image = "images/background.jpg"
+        self.caption  = "Alien Invasion by PMLeite"
+        self.ship_speed = 0.5
+
+        # configurações dos tiros
+        self.bullet_speed  = 1.0
+        self.bullet_width  = 5
+        self.bullet_height = 10
+        self.bullet_color  = (51, 255, 51)
