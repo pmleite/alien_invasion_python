6850ef4fd7ef91438ae41d51f8fd1207ac2f2c80
pmleite <paulo.miguel.r.leite@gmail.com>
2023-02-01 09:16:35 -0100

    update

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings:
     def __init__(self):
         self.screen_width  = 0
         self.screen_heigth = 0
-        self.bg_image      = "images/background.jpeg"
+        self.bg_image      = "images/background.jpg"
         self.alien_image   = "images/alien.png"
         self.ship_image    = "images/ship.png"
         self.caption       = "Alien Invasion by Paulo Leite"
