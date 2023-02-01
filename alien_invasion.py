aa975321fa1baf474115e93226a9f9e63b6a2a1a
pmleite <paulo.miguel.r.leite@gmail.com>
2023-02-01 12:05:23 -0100

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,6 +32,7 @@ class AlienInvasion:
             self._update_screen()
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
 
     def _update_bullets(self):
@@ -102,6 +103,21 @@ class AlienInvasion:
             alien.rect.x = alien.x
             alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
             self.aliens.add(alien)
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
+
+
+
+
 
     def _update_screen(self):
         self.screen.blit(self.bg_image, (0,0))     
