ea0c52f4e41af8c2d57e7bf9babe8fd1961edad5
pmleite <paulo.miguel.r.leite@gmail.com>
2023-02-01 11:51:26 -0100

    update

--- settings.py
+++ settings.py
@@ -15,5 +15,7 @@ class Settings:
         self.bullet_color    = (83,220,83)
         self.bullets_allowed = 10
 
-        self.alien_speed     = 1.0
+        self.alien_speed      = 1.0
+        self.fleet_drop_speed = 10
+        self.fleet_direction  = 1
  
