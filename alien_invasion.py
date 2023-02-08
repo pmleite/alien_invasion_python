704ec6d6d5f449713cf4a85959128062e1b629a9
pmleite <paulo.miguel.r.leite@gmail.com>
2023-02-08 09:34:43 -0100

    update

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,24 +1,30 @@
 import sys
+from time import sleep
 import pygame
 from settings import Settings
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from game_stats import GameStats
+
 
 class AlienInvasion:
 
     def __init__(self):
         
         pygame.init()
-        self.settings = Settings()
-        self.bg_image = pygame.image.load(self.settings.bg_image)
-        self.screen   = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.settings   = Settings()
+        self.bg_image   = pygame.image.load(self.settings.bg_image)
+        self.screen     = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
         self.settings.screen_width  = self.screen.get_rect().width
         self.settings.screen_heigth = self.screen.get_rect().height
+
         pygame.display.set_caption(self.settings.caption)
-        self.ship     = Ship(self)
-        self.bullets  = pygame.sprite.Group()
-        self.aliens   = pygame.sprite.Group()
+        self.stats      = GameStats(self)
+        self.ship       = Ship(self)
+        self.bullets    = pygame.sprite.Group()
+        self.aliens     = pygame.sprite.Group()
 
         self._create_fleet()
 
@@ -30,29 +36,61 @@ class AlienInvasion:
             self._update_bullets()
             self._update_aliens()
             self._update_screen()
+    
+    def _ship_hit(self):
+        self.stats.ships_left -= 1
+        
+        self.aliens.empty()
+        self.bullets.empty()
+
+        self._create_fleet()
+        self.ship.center_ship()
+
+        sleep(0.5)
+
+    def _check_aliens_bottom(self):
+        screen_rect = self.screen.get_rect()
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= screen_rect.bottom:
+                self._ship_hit()
+                break
+
 
     def _update_aliens(self):
         self._check_fleet_edges()
         self.aliens.update()
-        if pygame.sprite.spritecollideany(self.ship, self.aliens):
-            print("bateu")
+
+        if pygame.sprite.spritecollideany(self.ship,
+                                          self.aliens):
+            self._ship_hit()
+        self._check_aliens_bottom()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+    
+    def _change_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_bullets(self):
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <=0:
-                self.bullets.remove(bullet)
-        self._check_bullt_alien_collisons()
+                self.bullets.remove(bullet) 
+        self._check_bullet_alien_collisions()
 
-
-    def _check_bullt_alien_collisons(self):
-        collisions = pygame.sprite.groupcollide(self.bullets,
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(
+                                                self.bullets,
                                                 self.aliens,
                                                 True,
-                                                True)
+                                                True)  
         if not self.aliens:
             self.bullets.empty()
-            self._create_fleet()
-
+            self._create_fleet()   
 
     def _check_events(self):
             for event in pygame.event.get():
@@ -90,11 +128,11 @@ class AlienInvasion:
 
 
     def _create_fleet(self):
+
         alien = Alien(self)
         alien_width, alien_height = alien.rect.size
         available_space_x = self.settings.screen_width - (2 * alien_width)
         number_aliens_x   = available_space_x // (2 * alien_width)
-
         ship_height = self.ship.rect.height
         available_space_y = (self.settings.screen_heigth -
                              (3 * alien_height) - ship_height)
@@ -102,32 +140,15 @@ class AlienInvasion:
 
         for row_number in range(number_rows):
             for alien_number in range(number_aliens_x):
-                self._create_alien(alien_number, 
-                                   alien_width,
-                                   row_number, 
-                                   alien_height)
+                self._create_alien(alien_number, alien_width, row_number)
 
-    def _create_alien(self, alien_number,
-                            alien_width,
-                            row_number,
-                            alien_height):  
-                              
-            alien        = Alien(self)
-            alien.x      = alien_width + 2 * alien_width * alien_number
-            alien.rect.x = alien.x
-            alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
-            self.aliens.add(alien)
 
-    def _check_fleet_edges(self):
-        for alien in self.aliens.sprites():
-            if alien.check_edges():
-                self._change_fleet_direction()
-                break
-    
-    def _change_fleet_direction(self):
-        for alien in self.aliens.sprites():
-            alien.rect.y += self.settings.fleet_drop_speed
-        self.settings.fleet_direction *= -1
+    def _create_alien(self, alien_number, alien_width, row_number):
+        alien        = Alien(self)
+        alien.x      = alien_width + 2 * alien_width * alien_number
+        alien.rect.x = alien.x
+        alien.rect.y = alien.rect.height + 2 * alien.rect.height * row_number
+        self.aliens.add(alien)
 
     def _update_screen(self):
         self.screen.blit(self.bg_image, (0,0))     
--- settings.py
+++ settings.py
@@ -1,21 +1,25 @@
 class Settings:
 
     def __init__(self):
-        self.screen_width  = 0
-        self.screen_heigth = 0
-        self.bg_image      = "images/background.jpg"
-        self.alien_image   = "images/alien.png"
-        self.ship_image    = "images/ship.png"
-        self.caption       = "Alien Invasion by Paulo Leite"
-        self.ship_speed    = 0.8
+        self.screen_width       = 0
+        self.screen_heigth      = 0
+        self.bg_image           = "images/background.jpg"
+        self.alien_image        = "images/alien.png"
+        self.ship_image         = "images/ship.png"
+        self.caption            = "Alien Invasion by Paulo Leite"
+        self.ship_speed         = 0.8
 
-        self.bullet_speed    = 1.0
-        self.bullet_width    = 5
-        self.bullet_height   = 15
-        self.bullet_color    = (83,220,83)
-        self.bullets_allowed = 10
+        self.bullet_speed       = 1.0
+        self.bullet_width       = 2
+        self.bullet_height      = 25
+        self.bullet_color       = (83,220,83)
+        self.bullets_allowed    = 10
 
-        self.alien_speed      = 0.9
-        self.fleet_drop_speed = 5
-        self.fleet_direction  = 1
+        self.alien_speed        = 1.0
+        self.fleet_drop_speed   = 10
+        self.fleet_direction    = 1
+
+        self.ship_limit         = 3
+
+
  
